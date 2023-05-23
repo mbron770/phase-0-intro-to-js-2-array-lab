3b928abb781d297b17b0fde8a1ed2c315ab51beb
mbron770 <mbronfin.tsd@gmail.com>
2023-05-22 23:06:49 -0400

    add array functions per specs

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+// cats.push('cat')
+
+function destructivelyAppendCat(name) {
+    return cats.push(name); 
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name); 
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop(); 
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift(); 
+}
+
+function appendCat(name) {
+    const newCats = [...cats]
+    newCats.push(name)
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [...cats]
+    newCats.unshift(name)
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [...cats]
+    newCats.pop(name)
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.shift(name)
+    return newCats;
+}
